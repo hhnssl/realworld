2861145ed1cea24634e82d53897c9fd1286f9f8d
hhnssl <losnah5h@gmail.com>
2025-05-21 17:31:42 +0900

    chore: seed 추가 및 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO articles(author, title, slug, description, body, favorited, favorites_count)
+VALUES ('author1', 'title1','title1', 'description1', 'body1', false, 0);
+
+INSERT INTO articles(author, title, slug, description, body, favorited, favorites_count)
+VALUES ('author2', 'title2','title2', 'description2', 'body2', false, 0);
+
+INSERT INTO articles(author, title, slug, description, body, favorited, favorites_count)
+VALUES ('author2', 'title2-2','title2-2', 'description2-2', 'body2-2', false, 0);
+
+INSERT INTO articles(author, title, slug, description, body, favorited, favorites_count)
+VALUES ('author3', 'title3','title3', 'description3', 'body3', false, 0);
+
+INSERT INTO articles(author, title, slug, description, body, favorited, favorites_count)
+VALUES ('author4', 'title4','title4', 'description4', 'body4', false, 0);
